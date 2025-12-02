cd114be354598ae7fd25aa21f56f96c9978021d0
feruz <fmiboy@gmail.com>
2025-12-02 19:28:36 +0200

    add analytics notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -89,6 +89,7 @@ export default defineConfig({
           items: [
             { label: "Ecency SDK", slug: "developers/wallets" },
             { label: "Ecency chats", slug: "developers/chats" },
+            { label: "Analytics", slug: "developers/analytics" },
             { label: "API Reference", slug: "developers/api" }, // if you later add this
             {
               label: "Extra services",
